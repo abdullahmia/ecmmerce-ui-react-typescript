92a795f2c08239478fb691bb94cef1176426b900
abdullahmia <abdullah149161@bsdi-bd.org>
2022-04-04 01:21:50 +0600

    redux parsist

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
+import store, { persistor } from "./redux/store";
 import Main from "./views/pages/layout/Main";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Main />
+      <PersistGate loading={null} persistor={persistor}>
+        <Main />
+      </PersistGate>
     </Provider>
   )
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,18 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reduxThunk from 'redux-thunk';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import authReducer from './reducers/authReducer';
 import cartReducer from './reducers/cartReducer';
+import persistStore from 'redux-persist/es/persistStore';
+
+// redux parsist configuration
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
 
 // combine all of thoose reducers
 const rootReducer = combineReducers({
@@ -10,6 +20,8 @@ const rootReducer = combineReducers({
     auth: authReducer,
 })
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(reduxThunk)));
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
+const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(reduxThunk)));
+export const persistor = persistStore(store)
 export default store;
